0192b1d521ecc41c8a8745903c8f1b2d04deffd5
Allxie <allxiecleary@gmail.com>
2015-06-13 10:57:53 -0700

    add games to project page. Fix download link

--- app.js
+++ app.js
@@ -26,14 +26,24 @@ angular.module('myApp', [])
 }])
 
 .controller('HangManCtrl', ['$scope', function($scope){
-  $scope.currentWord = "angular";
+  $scope.inGame = false;
+  $scope.currentWord = "";
   $scope.hiddenCharls = [];
   $scope.win = " ";
   $scope.lose = " ";
   $("#hangDraw").html("<br>&nbsp;&nbsp;____<br>&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;|<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_|_<br>&nbsp;&nbsp;&nbsp;&nbsp;|___|");
 
   $scope.addWord = function(){
+    $scope.guesses = [];
+    $scope.inGame = true;
+    // Re makes the platform empty
+    $("#hangDraw").html("<br>&nbsp;&nbsp;____<br>&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;|<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_|_<br>&nbsp;&nbsp;&nbsp;&nbsp;|___|");
+    // changes the win and lose so that they're blank
+    $scope.win = " ";
+    $scope.lose = " ";
+    // grabs the new word
     $scope.currentWord = $scope.newWord;
+    // empties the form
     $scope.newWord = "";
     $scope.hiddenCharls = $scope.currentWord.split('');
     $scope.hiddenCharls.forEach(function(el,id,arr){
@@ -43,6 +53,11 @@ angular.module('myApp', [])
   };
 
 
+  var winOrLose = function(){
+    $scope.inGame = false;
+    // $scope.currentWord = "";
+    // $scope.hiddenCharls = [];
+  }
 
   $scope.guesses = [];
   $scope.guessLetter = function(){
@@ -72,7 +87,8 @@ angular.module('myApp', [])
             break;
         case 6:
             $("#hangDraw").html("<br>&nbsp;&nbsp;____<br>&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;|<br>\\0/&nbsp;&nbsp;&nbsp;|<br>&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;|<br>/&nbsp;\\&nbsp;&nbsp;_|_<br>&nbsp;&nbsp;&nbsp;&nbsp;|___|");
-            $scope.lose = "You LOSE. And you're DEAD.";
+            $scope.lose = "You LOSE. And you're DEAD. The word was \"" + $scope.currentWord + ".\"";
+            winOrLose();
             break;
         }
       }
@@ -95,7 +111,94 @@ angular.module('myApp', [])
     $scope.letter = "";
     console.log("scope Word array: ", $scope.hiddenCharls);
     if ($scope.hiddenCharls.indexOf("_") === -1){
+      winOrLose();
       $scope.win = "You Won!";
     }
   };
+}])
+.controller('DiceCtrl',['$scope', function($scope){
+  console.log("tis loading");
+
+  $scope.rollBut = function(){
+    console.log("clicked");
+    //grabs the three forms
+    var numDice = $scope.numDice;
+    var numSides = $scope.numSides;
+    var constant = $scope.constant;
+    // var numDice = document.getElementById("numDice").value;
+    // var numSides = document.getElementById("numSides").value;
+    // var constant = document.getElementById("constant").value;
+    var roll = new Roll(constant);
+    roll.init(numDice, numSides);
+    roll.roll();
+    console.log("sum :" + roll.sum);
+    //puts the sum where we want it.
+    $scope.rollsum = roll.sum;
+    // document.getElementById("rollsum").innerHTML = roll.sum;
+  };
+
+    //Using object oriented programming
+  //to create dice with numbers of sides
+
+  //Die constructor with a number of sides
+  function Die(numSides){
+    this.numSides = numSides;
+    this.result = 0;
+  }
+  //This prototype rolls a Die
+  Die.prototype.roll = function(){
+    var numSides = this.numSides;
+    this.result = Math.ceil(Math.random() * numSides);
+    return this.result;
+  }
+
+  //this is a constructor for a roll
+  function Roll(mod, dice){
+    this.dice = dice || [];
+    this.sum = 0;
+    this.mod = mod || 0;
+  }
+
+  //This rolls the dice and creates
+  Roll.prototype.roll = function(){
+    this.sum = 0;
+    $scope.individual = "";
+    // document.getElementById("individual").innerHTML = "";
+    var _this = this;
+    this.dice.forEach(function(die){
+
+      die.roll();
+      console.log("result: " + die.result);
+      console.log("sum 1 :" + _this.sum);
+      console.log(typeof die.result);
+      $scope.individual = $scope.individual + die.result + "  +  ";
+      // document.getElementById("individual").innerHTML += die.result + "  +  ";
+      _this.sum += die.result;
+      return _this.sum;
+    });
+    console.log(typeof this.mod);
+    this.sum += Number(this.mod);
+    $scope.individual = $scope.individual + "Mod " + this.mod;
+    // document.getElementById("individual").innerHTML += "Mod " + this.mod;
+  };
+  //initiates the roll by creating the right number of dice of the size we want
+  Roll.prototype.init = function(numDice, numSides){
+    while(this.dice.length < numDice) {
+      var die = new Die(numSides);
+      this.dice.push(die);
+    };
+  };
+
+
+  //end dice speciic stuff
+  // // // // // // // // // // // // // // // //
+
+}])
+.controller('TacCtrl',['$scope', function($scope){
+//Listen for event on all squares of the board
+
+
+
+
 }]);
+
---
+++ diceObj.js
@@ -0,0 +1,139 @@
+//Using object oriented programming
+//to create dice with numbers of sides
+
+//Die constructor with a number of sides
+function Die(numSides){
+	this.numSides = numSides;
+	this.result = 0;
+}
+//This prototype rolls a Die
+Die.prototype.roll = function(){
+	var numSides = this.numSides;
+	this.result = Math.ceil(Math.random() * numSides);
+	return this.result;
+}
+
+//this is a constructor for a roll
+function Roll(mod, dice){
+	this.dice = dice || [];
+	this.sum = 0;
+	this.mod = mod || 0;
+}
+
+//This rolls the dice and creates
+Roll.prototype.roll = function(){
+	this.sum = 0;
+	document.getElementById("individual").innerHTML = "";
+	var _this = this;
+	this.dice.forEach(function(die){
+
+		die.roll();
+		console.log("result: " + die.result);
+		console.log("sum 1 :" + _this.sum);
+		console.log(typeof die.result);
+		document.getElementById("individual").innerHTML += die.result + "  +  ";
+		_this.sum += die.result;
+		return _this.sum;
+	});
+	console.log(typeof this.mod);
+	this.sum += Number(this.mod);
+	document.getElementById("individual").innerHTML += "Mod " + this.mod;
+};
+//initiates the roll by creating the right number of dice of the size we want
+Roll.prototype.init = function(numDice, numSides){
+	while(this.dice.length < numDice) {
+		var die = new Die(numSides);
+		this.dice.push(die);
+	};
+};
+
+
+//end dice speciic stuff
+// // // // // // // // // // // // // // // //
+
+window.onload = function(){
+
+
+
+
+//////////////////////////////////////////////
+	console.log("running");
+
+// MAKES THE ROLL BUTTON WORK on dice page and new character page
+// creates a roll
+// grabs the button that tells it to roll
+var rollBut = document.getElementById("rollBut");
+rollBut.onclick = function(){
+	console.log("clicked");
+	//grabs the three forms
+	var numDice = document.getElementById("numDice").value;
+	var numSides = document.getElementById("numSides").value;
+	var constant = document.getElementById("constant").value;
+	var roll = new Roll(constant);
+	roll.init(numDice, numSides);
+	roll.roll();
+	console.log("sum :" + roll.sum);
+	//puts the sum where we want it.
+	document.getElementById("rollsum").innerHTML = roll.sum;
+};
+
+
+//creates a box on the add character page
+var counter = 0;
+	//This is the name of the button we click to add
+	var el = document.getElementById("addBox");
+	console.log(el);
+
+	var form = document.getElementById("newCharForm"); //not sure I need this.
+	el.onclick = function(){
+//makes the box and connects it to the stat model
+		var stat = $("<div class='enterStat'>"
+									+ "Name: <input name='stats[" + counter + "][name]' type='text' placeholder='str, wis' id='statNameBox'>"
+									+ "Value: <input type='number'name='stats[" + counter + "][value]' id='stats-value-"+counter+"'></div>");
+		counter++;
+		$("#putBox").append(stat);
+	}
+
+// Person clicks Roll and Fill, you call Roll and Fill Function
+var rollfill = document.getElementById("rollfill");
+rollfill.onclick = function(){
+ console.log("CLICKED!");
+ // Define an iterator i
+	var i = 0;
+	// Start a while loop, while(true) and i is less than 100 -- loop won't break otherwise
+	while(true && (i < 100)){
+		// Use jQuery selector to select each one of the stats-value-i id's one after another
+		var $stat = $("#stats-value-"+i);
+		// if($stat), then: roll the dice and take the result and assign it to $stat.value(diceroll);
+		if($stat){
+			var numDice = document.getElementById("numDice").value;
+			var numSides = document.getElementById("numSides").value;
+			var constant = document.getElementById("constant").value;
+			var roll = new Roll(constant);
+			roll.init(numDice, numSides);
+			roll.roll();
+			//$stat.value = roll.sum;
+			$stat.val(roll.sum);
+			i++;
+			$stat = null;
+		// else (meaning jQuery did not find a stat field with id stat-value-i, then break;)
+		} else {
+			break;
+		};
+	}
+}
+
+
+
+
+
+
+
+
+
+} //end onload
+
+
+
+
+
